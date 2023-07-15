1a5a002637e3abe4cf95be5aab9bfe75f3012d37
Dženan Kreho <92307873+dkreho1@users.noreply.github.com>
2023-07-15 22:47:48 +0200

    Init za tastere napisan

---
+++ pushButtons.c
@@ -0,0 +1,13 @@
+#include "pushButtons.h"
+
+
+Pin switchMode = PF0;
+Pin switchString = P41;
+Pin switchTune = P40;
+
+
+void initPushButtons(void) {
+	gpio_set_mode(switchMode, PullUp_Input);
+	gpio_set_mode(switchString, PullUp_Input);
+	gpio_set_mode(switchTune, PullUp_Input);
+}
---
+++ pushButtons.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "gpio.h"
+
+
+extern Pin switchMode;
+extern Pin switchString;
+extern Pin switchTune;
+
+
+void initPushButtons(void);
