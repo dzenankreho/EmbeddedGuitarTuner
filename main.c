e17d19083b888220d36ae03a8eb72d7acefa056c
Dženan Kreho <92307873+dkreho1@users.noreply.github.com>
2023-07-15 21:28:29 +0200

    Korigovane zvucne komande i implementirano koristenje istih

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include "arm_const_structs.h"
 #include "soundCommands.h"
 
-#define MEASURE_EXEC_TIME_
 #define BUFFER_SIZE 16*DMA_BUFFER_SIZE
 #define MEAN_THRESHOLD 1e4f
 #define VAR_THRESHOLD 1e10f
@@ -13,12 +12,11 @@ OperationMode operationMode = LISTENING;
 
 float32_t lowerBounds[] = {77.8f, 103.8f, 138.6f, 185.0f, 233.1f, 311.1f};
 float32_t upperBounds[] = {87.3f, 116.5f, 155.6f, 207.7f, 261.6f, 349.2f};
+float32_t tunedStrings[] = {82.4f, 110.0f, 146.8f, 196.0f, 246.9f, 329.6f};
 
 volatile int16_t audio_chR = 0;
 volatile int16_t audio_chL = 0;
 
-float32_t y[BUFFER_SIZE] = { 0 };
-float32_t x[2 * DMA_BUFFER_SIZE] = { 0 };
 float32_t buffer[2 * BUFFER_SIZE] = { 0 };
 float32_t buffer2[BUFFER_SIZE] = { 0 };
 int cnt = 0;
@@ -29,6 +27,10 @@ float32_t mean = 0;
 float32_t var = 0;
 int32_t soundCommandCnt = 0;
 int string = -1;
+float32_t logHelper1 = 0, logHelper2 = 0;
+int32_t n = 0, nScaled = 0;
+int16_t* soundCommand = 0;
+int32_t soundCommandDuration  = 0;
 
 void DMA_HANDLER(void) {
     if (dstc_state(0)) {
@@ -80,10 +82,7 @@ void proces_buffer(void) {
 		for (i = 0; i < (DMA_BUFFER_SIZE); i++) {
 			audio_IN = (*rxbuf++);
 			audio_chL = (audio_IN & 0x0000FFFF);
-			x[2*i] = (float32_t)(audio_chL);
-			y[cnt] = x[2*i];
-			buffer[2*cnt] = x[2*i];
-			x[2*i+1] = 0.0f;
+			buffer[2*cnt] = (float32_t)(audio_chL);
 			buffer[2*cnt+1] = 0.0f;
 			cnt++;
 		}
@@ -92,8 +91,8 @@ void proces_buffer(void) {
 	switch (operationMode) {
 		case PLAYING_COMMAND:
 			for (i = 0; i < (DMA_BUFFER_SIZE); i++) {
-				*txbuf++ = ((popustiteZicu[soundCommandCnt] << 16 & 0xFFFF0000)) + (zategniteZicu[soundCommandCnt] & 0x0000FFFF);
-				if (++soundCommandCnt == DURATION) {
+				*txbuf++ = ((soundCommand[soundCommandCnt] << 16 & 0xFFFF0000)) + (soundCommand[soundCommandCnt] & 0x0000FFFF);
+				if (++soundCommandCnt == soundCommandDuration) {
 					operationMode = LISTENING;
 					break;
 				}
@@ -116,20 +115,36 @@ void proces_buffer(void) {
 
 
 int main (void) {
+	/*
+	// 1 2 4 6 7 9 10 3 3'
+	Pin pins[] = {P1B, P1C, PF7, P43, PF5, P44, P45, PF3, P7D};
 	int j = 0;
-#ifdef MEASURE_EXEC_TIME
-	gpio_set_mode(P15, Output);
-#endif
+	for (j = 0; j < 9; j++) {
+		gpio_set_mode(pins[j], Output);
+		gpio_set(pins[j], ((j<7)?(HIGH):(LOW)));
+	}
+	
+	gpio_set(pins[0], HIGH);
+	gpio_set(pins[1], LOW);
+	gpio_set(pins[2], LOW);
+	gpio_set(pins[3], LOW);
+	gpio_set(pins[4], LOW);
+	gpio_set(pins[5], HIGH);
+	gpio_set(pins[6], LOW);
+	
+	
+	while(1);
+	*/
+	
+	int j = 0;	
 		
 	audio_init(hz8000, mic_in, dma, DMA_HANDLER);
 
 	while (1) {
 		while (!(rx_buffer_full && tx_buffer_empty));
-			
-#ifdef MEASURE_EXEC_TIME
-		gpio_set(P15, HIGH);
-#endif	
+
 		proces_buffer();
+		
 		if (cnt == BUFFER_SIZE) {
 			arm_cfft_f32(&arm_cfft_sR_f32_len2048, buffer, 0, 1);
 			arm_cmplx_mag_f32(buffer, buffer2, BUFFER_SIZE);
@@ -140,21 +155,38 @@ int main (void) {
 			freq = (maxIndex / 1024.0f) * 4000.0f;
 			if (mean > MEAN_THRESHOLD && var > VAR_THRESHOLD && freq > 30.0f) {
 				for (j = 0; j < 6; j++) {
-					if ((lowerBounds[j] <= freq && upperBounds[j] >= freq) ||
-						(lowerBounds[j] <= freq / 2 && upperBounds[j] >= freq / 2)) {
-							string = 6 - j;
-							break;
-						}
+					if (lowerBounds[j] <= freq && upperBounds[j] >= freq) {
+						string = 6 - j;
+						break;
+					}
+					if (lowerBounds[j] <= freq / 2 && upperBounds[j] >= freq / 2) {
+						freq /= 2;
+						string = 6 - j;
+						break;
+					}
 				}
-				soundCommandCnt = 0;
-				operationMode = PLAYING_COMMAND;
+				if (string != -1) {
+					logHelper1 = freq / tunedStrings[j];
+					arm_vlog_f32(&logHelper1, &logHelper2, 1);
+					n = 1731.23404907f * logHelper2;
+					nScaled = n / 10;
+					soundCommandCnt = 0;
+					if (nScaled < 0) {
+						soundCommand = zategniteZicu;
+						soundCommandDuration = ZATEGNITE_ZICU_DURATION;
+					} else if (nScaled > 0) {
+						soundCommand = popustiteZicu;
+						soundCommandDuration = POPUSTITE_ZICU_DURATION;
+					} else {
+						soundCommand = zicaNastimana;
+						soundCommandDuration = ZICA_NASTIMANA_DURATION;
+					}	
+					operationMode = PLAYING_COMMAND;
+				}
 			} else {
 				freq = -1;
 				string = -1;
 			}
 		}
-#ifdef MEASURE_EXEC_TIME
-		gpio_set(P15, LOW);
-#endif
 	}
 }
